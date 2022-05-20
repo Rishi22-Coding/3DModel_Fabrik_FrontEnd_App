059d33477d2d72ced12152cecbfb9b068b56eeb9
Rishi22-Coding <saptarshi.patra.22@gmail.com>
2022-05-20 23:43:42 +0530

    IPhone 3D Model Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './component/NavBar';
+import {Canvas} from "@react-three/fiber";
+import { OrbitControls } from '@react-three/drei';
+import { Suspense } from 'react';
+import IPhone from "../src/component/IPhone"
 
 function App() {
   return (
     <>
-      <NavBar />
+      <NavBar title={"3D Model Viewer Application"} />
+        <Canvas className="Iphone-Canvas" style={{paddingTop: 1, paddingRight: 1200, height: "500px"}} >
+          <OrbitControls enableZoom={false} />
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[-2, 5, 2]} intensity={1} />
+          <Suspense fallback={null}>
+            <IPhone />
+          </Suspense>
+        </Canvas>
     </>
   );
 }
