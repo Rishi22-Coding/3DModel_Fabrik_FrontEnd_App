c0a6eec290561dc24aaca0746bf26a3038ae06d0
Rishi22-Coding <saptarshi.patra.22@gmail.com>
2022-05-27 22:50:56 +0530

    Display View Changed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,12 @@ import { Center, OrbitControls } from "@react-three/drei";
 import React, { Suspense } from "react";
 import styled from "styled-components";
 import Iphone from "../components/threeJS/Iphone.js";
-import Cheerios from "../components/threeJS/Cheerios.js"
-import './style.css'
+import Cheerios from "../components/threeJS/Cheerios.js";
+import "./style.css";
 
 function Home() {
   return (
-    <div className="home">
+    <>
       <div className="row d-flex justify-content-center text-center">
         <label for="formFile" className="form-label" style={{ fontSize: 30 }}>
           Upload Your 3d Model
@@ -20,53 +20,62 @@ function Home() {
           style={{ width: 400 }}
         />
       </div>
-      <Wrapper className="styled">
-        <Canvas className="canvasIphone">
-          <OrbitControls enableZoom={false} />
-          <ambientLight intensity={1.3} />
-          <directionalLight position={[-2, 5, 2]} intensity={-1} />
-          <Suspense fallback={null}>
-            <Iphone />
-          </Suspense>
-        </Canvas>
-        <Canvas className="canvasCheerios">
-          <OrbitControls enableZoom={false} />
-          <ambientLight intensity={1.3} />
-          <directionalLight position={[-2, 5, 2]} intensity={-1} />
-          <Suspense fallback={null}>
-            <Cheerios />
-          </Suspense>
-        </Canvas>
-        <Canvas className="canvasIphone">
-          <OrbitControls enableZoom={false} />
-          <ambientLight intensity={1.3} />
-          <directionalLight position={[-2, 5, 2]} intensity={-1} />
-          <Suspense fallback={null}>
-            <Iphone />
-          </Suspense>
-        </Canvas>
-        <Canvas className="canvasCheerios">
-          <OrbitControls enableZoom={false} />
-          <ambientLight intensity={1.3} />
-          <directionalLight position={[-2, 5, 2]} intensity={-1} />
-          <Suspense fallback={null}>
-            <Cheerios />
-          </Suspense>
-        </Canvas>
-      </Wrapper>
-    </div>
+      <br />
+      <div className="container px-5">
+        <div className="row px-5 mx-5">
+          <div className="col-md-2 mx-5">
+            <div className="card mx-5" style={{ width: "16rem" }}>
+              <Canvas className="canvasIphone">
+                <OrbitControls enableZoom={false} />
+                <ambientLight intensity={1.3} />
+                <directionalLight position={[-2, 5, 2]} intensity={-1} />
+                <Suspense fallback={null}>
+                  <Iphone />
+                </Suspense>
+              </Canvas>
+              <div className="card-body">
+                <h5 className="card-title">IPhone</h5>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-2 mx-5">
+            <div className="card mx-5" style={{ width: "16rem" }}>
+              <Canvas className="canvasIphone">
+                <OrbitControls enableZoom={false} />
+                <ambientLight intensity={1.3} />
+                <directionalLight position={[-2, 5, 2]} intensity={-1} />
+                <Suspense fallback={null}>
+                  <Cheerios />
+                </Suspense>
+              </Canvas>
+              <div className="card-body">
+                <h5 className="card-title">IPhone</h5>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-2 mx-5">
+            <div className="card mx-5" style={{ width: "16rem" }}>
+              <Canvas className="canvasIphone">
+                <OrbitControls enableZoom={false} />
+                <ambientLight intensity={1.3} />
+                <directionalLight position={[-2, 5, 2]} intensity={-1} />
+                <Suspense fallback={null}>
+                  <Cheerios />
+                </Suspense>
+              </Canvas>
+              <div className="card-body">
+                <h5 className="card-title">IPhone</h5>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+      </>
   );
 }
 
-const Wrapper = styled.div`
-  position: relative;
-
-  canvasIphone {
-    height: 800px;
-  }
-  canvasCheerios {
-    height: 1000px;
-  }
-`;
 
 export default Home;
